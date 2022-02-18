9799c096791a5d64115e3691c31b937c6dc937ea
Artem <artem.k.sergienko@gmail.com>
2022-02-19 01:03:19 +0300

    Change name.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,12 @@
 module.exports = {
-    content: [
-      './public/**/*.{html,js}',
-      './src/**/*.{html,js}'
-    ],
-    theme: {
-      extend: {
-        colors: {
+  content: ["./public/**/*.{html,js}", "./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
         primary: "#303920",
         secondary: "#A7AC5E",
-        },
       },
     },
-    plugins: [],
-  }
+  },
+  plugins: [],
+};
